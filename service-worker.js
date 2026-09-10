0e267a30976e4af063ea3f4065ddeacba8c7e6c5
EastDragon72 <eastdragon.project@gmail.com>
2026-09-10 21:27:48 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-const CACHE="record-pwa-v3";const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="record-pwa-v5";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin && ["document","script","style"].includes(e.request.destination)){
+    e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;}).catch(()=>caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
